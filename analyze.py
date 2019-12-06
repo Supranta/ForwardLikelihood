1304ba72bcb8cd997a4d8b4dc78c06d978fd9a44
SuprantaSB <ssarmabo@uwaterloo.ca>
2019-12-06 15:10:22 -0500

    Changed the analysis script to plot different corner and chain plots for different catalogs

--- analyze.py
+++ analyze.py
@@ -41,29 +41,41 @@ samples = chain[:,N_BURN_IN:,:].reshape((-1,N_DIM))
 f = open(output_dir+'/results.txt','w')
 print('Writing results')
 for i in range(N_FLOW_PARAMS):
-        f.write(simple_labels[i]+': %2.3f +/- %2.3f \n' %(np.mean(samples[:,i]),np.std(samples[:,i])))
+    f.write(simple_labels[i]+': %2.3f +/- %2.3f \n' %(np.mean(samples[:,i]),np.std(samples[:,i])))
+if(plot_chain):
+    print('Plotting Flow model chain....')
+    flow_params = chain[:,:,:N_FLOW_PARAMS]
+    chain_plot(flow_params, labels=flow_model_labels, savefig=output_dir+'/flow_model_chain.png', show=False)
+if(plot_corner):
+    print('Plotting Flow model corner plot....')
+    flow_samples = samples[:,:N_FLOW_PARAMS]
+    corner.corner(flow_samples, labels=flow_model_labels)
+    plt.savefig(output_dir+'/flow_model_corner.png',dpi=150)
+
 N = N_FLOW_PARAMS
+
 for i in range(NCAT):
-        f.write('\n Catalog %d: \n'%(i))
-        catalog = catalogs[i]
-        if catalog[0]=='simple_gaussian':
-            v_data_type, rescale_distance, add_sigma_int, v_data_file = catalog
-            N_PARAMS = num_params(v_data_type, rescale_distance)
-        else:
-            v_data_type, v_data_file = catalog
-            N_PARAMS = num_params(v_data_type)
-        for j in range(N_PARAMS):
-            f.write('parameter %d: %2.3f +/- %2.3f \n' %(j, np.mean(samples[:,i+N]),np.std(samples[:,i+N])))
-        N += N_PARAMS
+    f.write('\n Catalog %d: \n'%(i))
+    catalog = catalogs[i]
+    if catalog[0]=='simple_gaussian':
+        v_data_type, rescale_distance, add_sigma_int, v_data_file = catalog
+        N_PARAMS = num_params(v_data_type, rescale_distance)
+    else:
+        v_data_type, v_data_file = catalog
+        N_PARAMS = num_params(v_data_type)
+    for j in range(N_PARAMS):
+        f.write('parameter %d: %2.3f +/- %2.3f \n' %(j, np.mean(samples[:,i+N]),np.std(samples[:,i+N])))
+    if(N_PARAMS != 0):
+        if(plot_chain):
+            print('Plotting catalog %d chain....'%(i))
+            catalog_params = chain[:,:,N:N+N_PARAMS]
+            chain_plot(catalog_params, savefig=output_dir+'/catalog_'+str(i)+'_chain.png', show=False)
+        if(plot_corner):
+            print('Plotting catalog %d corner plot....'%(i))
+            catalog_samples = samples[:,N:N+N_PARAMS]
+            corner.corner(catalog_samples)
+            plt.savefig(output_dir+'/catalog_'+str(i)+'_corner.png',dpi=150)
+    N += N_PARAMS
 f.close()
 
-if(plot_chain):
-        print('Plotting Flow model chain....')
-        flow_params = chain[:,:,:N_FLOW_PARAMS]
-        chain_plot(flow_params, labels=flow_model_labels, savefig=output_dir+'/chain.png', show=False)
 
-if(plot_corner):
-        print('Plotting Flow model corner plot....')
-        flow_samples = samples[:,:N_FLOW_PARAMS]
-        corner.corner(flow_samples, labels=flow_model_labels)
-        plt.savefig(output_dir+'/corner.png',dpi=150)
